f80b7acb0e1dd47832367fe8a5f3c7e4dc61c74e
Evgeny Novikov <evgeniy.develop@yandex.ru>
2023-09-28 12:39:17 +0300

    fix: правки после ревью

--- src/pages/profile/password-change/password-change.tmpl.ts
+++ src/pages/profile/password-change/password-change.tmpl.ts
@@ -7,17 +7,19 @@ export const passwordChangeTmpl = `
             {{{avatar}}}
             <h1 class="${style.title}">{{firstName}}</h1>
         </div>
-        <ul class="${style.list}">
-            {{#each passwordItems}}
-                <li class="${style.field} ${style.list_item}">
-                    <label for="{{name}}">{{title}}</label>
-                    <input type="password" class="${style.info_input}" id="{{name}}" name="{{name}}" value="{{value}}" />
-                </li>
-            {{/each}}
-        </ul>
-        <div class="${style.button_wrapper}">
-            {{{button}}}
-        </div>
+        <form class="${style.form}">
+            <ul class="${style.list}">
+                {{#each passwordItems}}
+                    <li class="${style.field} ${style.list_item}">
+                        <label for="{{name}}">{{title}}</label>
+                        <input type="password" class="${style.info_input}" id="{{name}}" name="{{name}}" value="{{value}}" />
+                    </li>
+                {{/each}}
+            </ul>
+            <div class="${style.button_wrapper}">
+                {{{button}}}
+            </div>
+        </form>
     </div>
 `;
 
--- src/pages/profile/profile-change/profile-change.tmpl.ts
+++ src/pages/profile/profile-change/profile-change.tmpl.ts
@@ -7,17 +7,19 @@ export const main = `
             {{{avatar}}}
             <h1 class="${style.title}">{{firstName}}</h1>
         </div>
-        <ul class="${style.list}">
-            {{#each profileInfo}}
-                <li class="${style.field} ${style.list_item}">
-                    <label for="{{name}}">{{title}}</label>
-                    <input class="${style.info_input}" id="{{name}}" name="{{name}}" value="{{value}}" />
-                </li>
-            {{/each}}
-        </ul>
-        <div class="${style.button_wrapper}">
-            {{{button}}}
-        </div>
+        <form class="${style.form}">
+            <ul class="${style.list}">
+                {{#each profileInfo}}
+                    <li class="${style.field} ${style.list_item}">
+                        <label for="{{name}}">{{title}}</label>
+                        <input class="${style.info_input}" id="{{name}}" name="{{name}}" value="{{value}}" />
+                    </li>
+                {{/each}}
+            </ul>
+            <div class="${style.button_wrapper}">
+                {{{button}}}
+            </div>
+        </form>
     </div>
 `;
 
